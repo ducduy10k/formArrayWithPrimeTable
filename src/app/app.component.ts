8078788b6a72f5679b4e2af4920a94e1804fa049
ducduy10k <ducduy10k@gmail.com>
2023-04-01 00:01:47 +0700

    init ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { Observable, timer } from 'rxjs';
+
+interface Product {
+  id: string;
+  code: string;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+  category: string;
+  quantity: number;
+  inventoryStatus: string;
+  rating: number;
+}
+
+interface SelectItem {
+  label: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'formArrayWithPrimeTable';
+
+  products: Product[] = [];
+
+  statuses: SelectItem[] = [];
+
+  clonedProducts: { [s: string]: Product } = {};
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    new Observable<Product[]>((obs) => {
+      timer(1000).subscribe(() => {
+        obs.next([
+          {
+            id: '1000',
+            code: 'f230fh0g3',
+            name: 'Bamboo Watch',
+            description: 'Product Description',
+            image: 'bamboo-watch.jpg',
+            price: 65,
+            category: 'Accessories',
+            quantity: 24,
+            inventoryStatus: 'INSTOCK',
+            rating: 5,
+          },
+        ]);
+      });
+    }).subscribe((data: Product[]) => {
+      this.products = data;
+    });
+    this.statuses = [
+      { label: 'In Stock', value: 'INSTOCK' },
+      { label: 'Low Stock', value: 'LOWSTOCK' },
+      { label: 'Out of Stock', value: 'OUTOFSTOCK' },
+    ];
+  }
+
+  onRowEditInit(product: Product) {
+    this.clonedProducts[product.id] = { ...product };
+  }
+
+  onRowEditSave(product: Product) {
+    if (product.price > 0) {
+      delete this.clonedProducts[product.id];
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Product is updated',
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Invalid Price',
+      });
+    }
+  }
+
+  onRowEditCancel(product: Product, index: number) {
+    this.products[index] = this.clonedProducts[product.id];
+    delete this.clonedProducts[product.id];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MessageService } from 'primeng/api';
+import { TableModule } from 'primeng/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TableModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    ToastModule,
+    ButtonModule,
+    InputTextModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MessageService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
